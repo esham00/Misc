d931ff6c39d883e2bb876b1c7319024ea7f2b377
Elizabeth Sham <esham00@stuy.edu>
2019-02-05 22:52:08 -0500

    works

--- makeAllWords.java
+++ makeAllWords.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class makeAllWords{
     public static List<String> makeAllWords(int k, int maxLetter) {
 	List<String> output = new ArrayList<String>(10);
-        for(int i = 97; i < maxLetter;i++) {
+        for(int i = 97; i <= maxLetter;i++) {
 	    helper(k-1, (char)i, 97,  maxLetter, output);
 	}
 	return output;
@@ -10,20 +10,20 @@ public class makeAllWords{
     public static void helper(int k, char constant,  int start, int  maxLetter, List<String> output) {
 	String word = constant + "";
 	if ((maxLetter - start) + 1 == k) {
-	    for (int i = start; i < k; i++) {
-		word += (char)i;
+	    for (int i = start; i < k + start; i++) {
+		word = word + (char)i;
 	    }
 	    output.add(word);
 	} else {
-	    for(int i = start; i < k; i++) {
-		word += (char)i;
+	    for(int i = start; i < start + k; i++) {
+		word = word + (char)i;
 	    }
 	    output.add(word);
 	    helper(k, constant, start+1, maxLetter, output);
 	}
     }
     public static void main(String[] args) {
-	System.out.println(makeAllWords(1,3));
-	System.out.println(makeAllWords(2,3));
+	List<String> output = new ArrayList<String>(10);
+	System.out.println(makeAllWords(2,99));
     }
 }
