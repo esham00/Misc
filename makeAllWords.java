9d5fd0a84cebf776d6ba3da36f88bdd47b5cde70
Elizabeth Sham <esham00@stuy.edu>
2019-02-05 23:05:26 -0500

    jk only works w k = 2'

--- makeAllWords.java
+++ makeAllWords.java
@@ -3,27 +3,32 @@ public class makeAllWords{
     public static List<String> makeAllWords(int k, int maxLetter) {
 	List<String> output = new ArrayList<String>(10);
         for(int i = 97; i <= maxLetter;i++) {
-	    helper(k-1, (char)i, 97,  maxLetter, output);
+	    helper(k-1, (char)i + "", 97,  maxLetter, output, k-1, (char)i + "");
 	}
 	return output;
     }
-    public static void helper(int k, char constant,  int start, int  maxLetter, List<String> output) {
-	String word = constant + "";
-	if ((maxLetter - start) + 1 == k) {
+    public static void helper(int k, String constant,  int start, int  maxLetter, List<String> output, int originalK, String originalC) {
+	String word = constant;
+	if ((maxLetter - start) + 1 == originalk) {
 	    for (int i = start; i < k + start; i++) {
 		word = word + (char)i;
 	    }
 	    output.add(word);
 	} else {
-	    for(int i = start; i < start + k; i++) {
+	    for(int i = start; i < start + originalk; i++) {
 		word = word + (char)i;
 	    }
 	    output.add(word);
-	    helper(k, constant, start+1, maxLetter, output);
+	    if (k > 0) {
+		helper(k-1, constant, start, maxLetter, output, originalK);
+	    } else {
+		helper(originalK, originalC, start+1, maxLetter, output, originalk, originalC);
+	    }
 	}
     }
     public static void main(String[] args) {
 	List<String> output = new ArrayList<String>(10);
 	System.out.println(makeAllWords(2,99));
+	System.out.println(makeAllWords(3,99));
     }
 }
